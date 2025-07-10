e07663eb3ddb93a1ffe24abf807f0f537d50f60d
Bashiru200 <bashirubarrie12@gmail.com>
2025-07-10 08:54:48 -0400

    refactor command

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,11 +1,9 @@
-SELECT * FROM User WHERE email = 'john@example.com';
-SELECT * FROM User WHERE email = 'john@example.com';
-
-
 CREATE INDEX indx_user_email on User(email);
 Create INDEX indx_property_review on Property(location);
 Create INDEX indx_booking_total_price on Booking(total_price);
 
-EXPLAIN SELECT * FROM User WHERE email = 'john@example.com';
-
-EXPLAIN ANALYZE SELECT * FROM User WHERE email = 'john@example.com';
+EXPLAIN ANALYZE 
+    SELECT 
+        User.email  
+    FROM User  
+    WHERE email = 'john@example.com';
